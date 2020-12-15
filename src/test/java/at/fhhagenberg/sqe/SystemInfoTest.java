45e9dd916adeff2a68f7a1d6149be5aeebf0df52
Sajan <S1910567014@students.fh-hagenberg.at>
2020-12-15 18:24:41 +0100

    changed expected java fx version

--- src/test/java/at/fhhagenberg/sqe/SystemInfoTest.java
+++ src/test/java/at/fhhagenberg/sqe/SystemInfoTest.java
@@ -7,11 +7,11 @@ import org.junit.jupiter.api.Test;
 public class SystemInfoTest {
     @Test
     public void testJavaVersion() {
-        assertEquals("11.0.9", SystemInfo.javaVersion());
+        assertEquals("14.0.2", SystemInfo.javaVersion());
     }
 
     @Test
     public void testJavafxVersion() {
-        assertEquals("13", SystemInfo.javafxVersion());
+        assertEquals("11.0.9", SystemInfo.javafxVersion());
     }
 }
